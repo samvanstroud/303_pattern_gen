28528d0d5dec8355eeef3ae1b207e02e5a78af33
samvanstroud <sam.van.stroud@cern.ch>
2021-01-28 10:46:48 +0000

    update common files

--- common.py
+++ common.py
@@ -1,3 +1,9 @@
+note_default = -1
+gate_default = 0
+accent_default = 0
+tie_default = 0
+slide_default = 0
+
 # need a single character to represent each note
 note_value_to_char = {
     0: 'C',
@@ -12,4 +18,19 @@ note_value_to_char = {
     9: 'A',
     10: 'b',
     11: 'B',
+}
+
+char_to_note_value = {
+    'C': 0,
+    'd': 1,
+    'D': 2,
+    'e': 3,
+    'E': 4,
+    'F': 5,
+    'g': 6,
+    'G': 7,
+    'a': 8,
+    'A': 9,
+    'b': 10,
+    'B': 11,
 }
